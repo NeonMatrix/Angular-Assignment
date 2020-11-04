14b2cf2948683ce2db69dd6fe59350eb19c8156d
povkub@gmail.com <povkub@gmail.com>
2020-11-04 09:27:22 +0000

    angular app says 'Hello world

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <to-do-list></to-do-list>
+  `
 })
 export class AppComponent {
   title = 'todo-app';
